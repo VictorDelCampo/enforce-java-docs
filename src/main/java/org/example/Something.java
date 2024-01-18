394cc9724abb9f31e4758f535c2b67798becc182
Victor del Campo <victor.delcampo@digitalservice.bund.de>
2024-01-18 16:11:25 +0100

    Test

--- src/main/java/org/example/Something.java
+++ src/main/java/org/example/Something.java
@@ -1,8 +1,16 @@
 package org.example;
 
 
+/**
+ * Test
+ */
 public class Something {
 
+    /**
+     * Test comment
+     * @param test
+     * @return the return test
+     */
     public String doSth(int test) {
         System.out.println("Do sth");
         return "test";
